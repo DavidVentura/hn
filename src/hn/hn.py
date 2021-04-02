3f001c9ad7831a40ba7f5e95ac39c6638477c746
david <davidventura27@gmail.com>
2021-04-02 23:07:20 +0200

    implement styling

--- src/hn/hn.py
+++ src/hn/hn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
 import queue
+from pathlib import Path
 
 import gi
 
@@ -12,12 +13,14 @@ from pango import html_to_pango
 from api import top_stories, get_id
 q = queue.Queue()
 
+STYLE_FILE = Path(__file__).parent / 'css' / 'style.css'
+
 class AppWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.connect("destroy", Gtk.main_quit)
+        self.connect("destroy", Gtk.main_quit)
+        self.setup_styles()
         self.set_default_size(360, 640)
-        self.set_border_width(10)
 
         self.stack = Gtk.Stack()
         self.stack.set_vexpand(True)
@@ -27,15 +30,22 @@ class AppWindow(Gtk.ApplicationWindow):
 
         self.ct = CommentThread()
         self.news_list = NewsList()
-        #label.connect('clicked', self.set_thread)
 
         self.stack.add(self.news_list)
         self.stack.add(self.ct)
-        #self.stack.set_visible_child(self.news_list)
 
         self.add(self.stack)
         self.show_all()
     
+    def setup_styles(self):
+        css_provider = Gtk.CssProvider()
+        context = Gtk.StyleContext()
+        screen = Gdk.Screen.get_default()
+
+        css_provider.load_from_path(str(STYLE_FILE))
+        context.add_provider_for_screen(screen, css_provider,
+                                        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
     def set_thread(self, thread_id):
         self.stack.set_visible_child(self.ct)
         self.ct.set_comments(thread_id)
@@ -53,7 +63,7 @@ class NewsList(Gtk.Grid):
         scrolled_window.set_vexpand(True)
 
         self.add(scrolled_window)
-        self.vbox = Gtk.VBox(spacing=6)
+        self.vbox = Gtk.VBox()
         self.vbox.set_homogeneous = False
         scrolled_window.add(self.vbox)
         self.show_all()
@@ -71,12 +81,15 @@ class CommentThread(Gtk.Grid):
         super().__init__(*args, **kwds)
 
         scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.get_style_context().add_class('thread-comments')
         scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         scrolled_window.set_hexpand(True)
 
         label = Gtk.Label(label='< Go back')
         label.set_hexpand(True)
         label.set_xalign(0)
+        label.get_style_context().add_class('thread-back')
+
         back_event = Gtk.EventBox()
         back_event.add(label)
         back_event.connect('button-release-event', self.back_click)
@@ -84,7 +97,7 @@ class CommentThread(Gtk.Grid):
         self.attach(back_event, 0, 0, 1, 1)
         self.attach(scrolled_window, 0, 1, 1, 10)
 
-        self.vbox = Gtk.VBox(spacing=6)
+        self.vbox = Gtk.VBox()
         self.vbox.set_homogeneous = False
         scrolled_window.add(self.vbox)
         self.show_all()
@@ -108,6 +121,7 @@ class CommentThread(Gtk.Grid):
 class NewsItem(Gtk.Grid):
     def __init__(self, _item_id, *args, **kwds):
         super().__init__(*args, **kwds)
+        self.get_style_context().add_class('news-item')
 
         self.set_column_homogeneous(True)
         self.article_url = None
@@ -161,28 +175,34 @@ class NewsItem(Gtk.Grid):
             url = data['url'].split('/')[2]
         else:
             url = 'self'
-        self.title.set_markup(f'<span size="14000">{title}</span>')
+        self.title.set_markup(f'<span size="12000">{title}</span>')
         self.url.set_label(url)
         self.comments.set_label(str(comment_count))
 
 
-class Comment(Gtk.Grid):
+class Comment(Gtk.VBox):
     def __init__(self, _item_id, *args, **kwds):
         super().__init__(*args, **kwds)
+
+        comment_body = Gtk.Grid()
+
+        comment_body.get_style_context().add_class('comment-item')
         self._time = Gtk.Label()
         self._time.set_margin_bottom(10)
         self._time.set_xalign(0)
-        self.attach(self._time, 1, 1, 1, 1)
+        comment_body.attach(self._time, 1, 1, 1, 1)
 
         self.comment = Gtk.Label(label='...')
         self.comment.set_vexpand(True)
         self.comment.set_line_wrap(True)
         self.comment.set_selectable(True)
         self.comment.set_xalign(0)
-        self.attach(self.comment, 1, 2, 20, 1)
+        comment_body.attach(self.comment, 1, 2, 20, 1)
 
         self.replies = Gtk.VBox(spacing=6)
-        self.attach(self.replies, 1, 3, 20, 10)
+        self.replies.get_style_context().add_class('comment-replies')
+        self.add(comment_body)
+        self.add(self.replies)
         self.show_all()
 
         q.put((self._set_content, _item_id))
@@ -202,7 +222,10 @@ class Comment(Gtk.Grid):
             text = comment['text']
             by = comment['by']
         dead = comment.get('dead', False)
-        self._time.set_markup(f"<span foreground='#999'>2m ago</span> - <b>{by}</b>")
+        if dead:
+            self.get_style_context().add_class('comment-item-dead')
+
+        self._time.set_markup(f"<small><span foreground='#999'>2m ago</span> - <b>{by}</b></small>")
 
         text = html_to_pango(text)
         self.comment.set_markup(text)
